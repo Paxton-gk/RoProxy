b5225683247b60af63828a0d213774f46bb888b7
Paxton Giesen-Kath <109930646+Paxton-gk@users.noreply.github.com>
2023-02-15 14:41:13 -0600

    Delete game.js

--- game.js
+++
@@ -1,8 +0,0 @@
-import { getURL } from "next/dist/shared/lib/utils";
-
-const url = "https://roblox.com/places/api-get-details?assetId=";
-const placeId = 6284583030
-
-export default function handler(req, res) {
-    res.status(200).json({message: "Hello There", amount: 50});
-}
